8516d43d658d38ca92829e1a56fcaddadaca1ad1
DJ Nelson <11001514+techied@users.noreply.github.com>
2022-07-22 22:06:15 +0000

    don't read serial

--- pihole-monitor/pihole-monitor.ino
+++ pihole-monitor/pihole-monitor.ino
@@ -120,16 +120,6 @@ void startAdv(void)
 
 void loop()
 {
-  // Forward data from HW Serial to BLEUART
-  while (Serial.available())
-  {
-    // Delay to wait for enough input, since we have a limited transmission buffer
-    delay(2);
-
-    uint8_t buf[64];
-    int count = Serial.readBytes(buf, sizeof(buf));
-    bleuart.write(buf, count);
-  }
 
   if (digitalRead(LEFT_BUTTON) == HIGH)
   {
