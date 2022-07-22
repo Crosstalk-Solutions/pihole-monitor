4a968f307f07915dfdec3de28624d3fd0ebdc3d7
DJ Nelson <11001514+techied@users.noreply.github.com>
2022-07-22 01:45:19 +0000

    text align

--- pihole-monitor/pihole-monitor.ino
+++ pihole-monitor/pihole-monitor.ino
@@ -162,13 +162,13 @@ void loop()
     strncat(recvText, &cha, 1);
     if (cha == '\n') {
       arcada.display->fillScreen(ARCADA_BLACK);
-      arcada.display->setCursor(0, 10);
+      arcada.display->setCursor(0, 20);
       String text = String(recvText);
       int index = text.indexOf(';');
       String numBlocked = text.substring(0, index);
       arcada.display->setTextSize(1);
       arcada.display->println("Domains Blocked");
-      arcada.display->setCursor(0, 30);
+      arcada.display->setCursor(0, 50);
       arcada.display->setTextSize(2);
       arcada.display->println(numBlocked);
       recvText[0]='\0';
