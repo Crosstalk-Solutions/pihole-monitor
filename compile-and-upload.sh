cb5083ffda97109af015e01f9f8cdb63a4527523
DJ Nelson <11001514+techied@users.noreply.github.com>
2022-07-22 00:59:02 +0000

    fix compile/upload script

--- compile-and-upload.sh
+++ compile-and-upload.sh
@@ -1,2 +1,2 @@
 arduino-cli compile -b adafruit:nrf52:cluenrf52840 pihole-monitor/
-upload -b adafruit:nrf52:cluenrf52840 -p /dev/ttyACM0 pihole-monitor/
+arduino-cli upload -b adafruit:nrf52:cluenrf52840 -p /dev/ttyACM0 pihole-monitor/
