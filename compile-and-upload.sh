2cf607f24bcaef3b62c0e7b9ebc06f39beb1fca9
DJ Nelson <11001514+techied@users.noreply.github.com>
2022-07-22 22:05:58 +0000

    read mac from serial

--- compile-and-upload.sh
+++ compile-and-upload.sh
@@ -1,2 +1,4 @@
 arduino-cli compile -b adafruit:nrf52:cluenrf52840 pihole-monitor/
 arduino-cli upload -b adafruit:nrf52:cluenrf52840 -p /dev/ttyACM0 pihole-monitor/
+read -r line < /dev/ttyACM0
+echo "Read MAC Address: $line"
--- pihole-monitor/pihole-monitor.ino
+++ pihole-monitor/pihole-monitor.ino
@@ -16,6 +16,12 @@
 #include <InternalFileSystem.h>
 #include "Adafruit_Arcada.h"
 #include <Fonts/FreeSansOblique9pt7b.h>
+typedef volatile uint32_t REG32;
+#define pREG32 (REG32 *)
+#define DEVICE_ID_HIGH (*(pREG32(0x10000060)))
+#define DEVICE_ID_LOW (*(pREG32(0x10000064)))
+#define MAC_ADDRESS_HIGH (*(pREG32(0x100000a8)))
+#define MAC_ADDRESS_LOW (*(pREG32(0x100000a4)))
 
 // Color definitions
 
@@ -38,10 +44,8 @@ int ads_over_time[150];
 #define BLACK 0x0000
 
 // BLE Service
-BLEDfu bledfu;   // OTA DFU service
-BLEDis bledis;   // device information
-BLEUart bleuart; // uart over ble
-BLEBas blebas;   // battery
+BLEDis bledis;
+BLEUart bleuart;
 
 Adafruit_Arcada arcada;
 
@@ -54,58 +58,37 @@ char recvText[1] = "";
 void setup()
 {
   Serial.begin(115200);
-#if CFG_DEBUG
-  // Blocking wait for connection when debug mode is enabled via IDE
-  while (!Serial)
-    yield();
-#endif
+  uint32_t addr_high = ((MAC_ADDRESS_HIGH)&0x0000ffff) | 0x0000c000;
+  uint32_t addr_low = MAC_ADDRESS_LOW;
+  Serial.print((addr_high >> 8) & 0xFF, HEX);
+  Serial.print(":");
+  Serial.print((addr_high)&0xFF, HEX);
+  Serial.print(":");
+  Serial.print((addr_low >> 24) & 0xFF, HEX);
+  Serial.print(":");
+  Serial.print((addr_low >> 16) & 0xFF, HEX);
+  Serial.print(":");
+  Serial.print((addr_low >> 8) & 0xFF, HEX);
+  Serial.print(":");
+  Serial.print((addr_low)&0xFF, HEX);
+  Serial.println("");
 
-  Serial.println("Bluefruit52 BLEUART Example");
-  Serial.println("---------------------------\n");
-
-  // Setup the BLE LED to be enabled on CONNECT
-  // Note: This is actually the default behavior, but provided
-  // here in case you want to control this LED manually via PIN 19
   Bluefruit.autoConnLed(true);
-
-  // Config the peripheral connection with maximum bandwidth
-  // more SRAM required by SoftDevice
-  // Note: All config***() function must be called before begin()
   Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
 
   Bluefruit.begin();
-  Bluefruit.setTxPower(4); // Check bluefruit.h for supported values
-  // Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple central connections
+  Bluefruit.setTxPower(4);
   Bluefruit.Periph.setConnectCallback(connect_callback);
   Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
-
-  // To be consistent OTA DFU should be added first if it exists
-  bledfu.begin();
-
-  // Configure and Start Device Information Service
   bledis.setManufacturer("Adafruit Industries");
   bledis.setModel("Bluefruit Feather52");
   bledis.begin();
-
-  // Configure and Start BLE Uart Service
   bleuart.begin();
-
-  // Start BLE Battery Service
-  blebas.begin();
-  blebas.write(100);
-
   apds.begin();
-
-  // gesture mode will be entered once proximity mode senses something close
   apds.enableProximity(true);
   apds.enableGesture(true);
-
-  // Set up and start advertising
   startAdv();
-
   arcada.displayBegin();
-
-  // Turn on backlight
   arcada.setBacklight(255);
   arcada.display->fillScreen(ARCADA_BLACK);
   arcada.display->setCursor(0, 10);
@@ -119,30 +102,20 @@ void setup()
 
 void startAdv(void)
 {
-  // Advertising packet
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
 
-  // Include bleuart 128-bit uuid
   Bluefruit.Advertising.addService(bleuart);
 
-  // Secondary Scan Response packet (optional)
-  // Since there is no room for 'Name' in Advertising packet
   Bluefruit.ScanResponse.addName();
 
-  /* Start Advertising
-     - Enable auto advertising if disconnected
-     - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-     - Timeout for fast mode is 30 seconds
-     - Start(timeout) with timeout = 0 will advertise forever (until connected)
-
-     For recommended advertising interval
+  /*
      https://developer.apple.com/library/content/qa/qa1931/_index.html
   */
   Bluefruit.Advertising.restartOnDisconnect(true);
-  Bluefruit.Advertising.setInterval(32, 244); // in unit of 0.625 ms
-  Bluefruit.Advertising.setFastTimeout(30);   // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);             // 0 = Don't stop advertising after n seconds
+  Bluefruit.Advertising.setInterval(32, 244);
+  Bluefruit.Advertising.setFastTimeout(30);
+  Bluefruit.Advertising.start(0);
 }
 
 void loop()
